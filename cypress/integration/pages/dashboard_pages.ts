1e802f6cc9d0bd4a53e0b3f411976e1682fa9197
Alfarau Ala <alfarau@qlue.id>
2022-12-28 14:44:31 +0700

    add new scenario and main.ts

--- cypress/integration/pages/dashboard_pages.ts
+++ cypress/integration/pages/dashboard_pages.ts
@@ -3,12 +3,14 @@ export class DashboardPage{
     link_sauceLabsBackpack = 'Sauce Labs Backpack'
     cart_sauceLabsBackpack = '#add-to-cart-sauce-labs-backpack' 
     btn_cart = '#shopping_cart_container'
+    btn_filter = '[class=product_sort_container]' 
 
     sauceLabsBackpack(){
         cy.contains(this.link_sauceLabsBackpack).click()
         cy.contains('Sauce Labs Backpack').should('be.visible') 
     }
     
+    ///CART
     addCartSauceLabsBackpack(){
         cy.get(this.cart_sauceLabsBackpack).click()
     }
@@ -16,6 +18,23 @@ export class DashboardPage{
     assertCartBackpack(){
         cy.get(this.btn_cart).click()
         cy.get('#item_4_title_link').should('be.visible')
-        cy.contains('Sauce Labs Backpack')
+        cy.contains('Sauce Labs Backpack').should('contain','Sauce Labs Backpack')
+    }
+
+    ///FILTER
+    filterNameZtoA(){
+        cy.get(this.btn_filter).select('za')
+    }
+
+    filterPriceHitoLo(){
+        cy.get(this.btn_filter).select('hilo')
+    }
+
+    assertFilterName(){
+        cy.get('#item_4_title_link').should('be.visible')
+    }
+
+    assertFilterPrice(){
+        cy.get('#item_5_title_link').should('be.visible')
     }
 }
