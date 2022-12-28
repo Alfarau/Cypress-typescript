1e609688b2cf1f5d4be3392b6207fe8098f419ec
Farau <118482404+Alfarau@users.noreply.github.com>
2022-12-28 10:57:27 +0700

    Update dashboard_pages.ts

--- cypress/integration/pages/dashboard_pages.ts
+++ cypress/integration/pages/dashboard_pages.ts
@@ -5,4 +5,14 @@ export class DashboardPage{
         cy.contains(this.link_sauceLabsBackpack).click()
         cy.contains('Sauce Labs Backpack').should('be.visible') 
     }
+    
+    addCartSauceLabsBackpack(){
+        cy.get(this.cart_sauceLabsBackpack).click()
+    }
+
+    assertCartBackpack(){
+        cy.get(this.btn_cart).click()
+        cy.get('#item_4_title_link').should('be.visible')
+        cy.contains('#item_4_title_link').should('contain', 'Sauce Labs Backpack')
+    }
 }
